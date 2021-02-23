828653ef979f6bfc63d84796ccae860a3ce5b526
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-23 11:47:28 -0300

    created CountDown Component and added its styles

---
+++ src/components/CountDown.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/CountDown.module.css'
+export default function CountDown() {
+    return (
+        <div className={styles.countDownContainer}>
+            <div>
+                <span>2</span>
+                <span>5</span>
+            </div>
+            <span>:</span>
+            <div>
+                <span>0</span>
+                <span>0</span>
+            </div>
+        </div>
+    )
+}
