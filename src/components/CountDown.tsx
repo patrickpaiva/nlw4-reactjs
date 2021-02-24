697c2b1a9b2cb4cd86a32b05c626803df7115f22
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-24 15:16:21 -0300

    created Context for the app

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengeContext'
 
 import styles from '../styles/components/CountDown.module.css'
 
 let countdownTimeout: NodeJS.Timeout
 
 export default function CountDown() {
+    const { startNewChalleng } = useContext(ChallengesContext)
     const [time, setTime] = useState(25 * 60)
     const [isActive, setIsActive] = useState(false)
     const [hasFinished, setHasFinished] = useState(false)
@@ -33,6 +35,7 @@ export default function CountDown() {
         } else if (isActive && time === 0) {
             setHasFinished(true)
             setIsActive(false)
+            startNewChalleng()
         }
     }, [isActive, time])
 
