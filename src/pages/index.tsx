b68d73ef64095be5306a3a78e80457b1a44d1b12
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-26 10:51:31 -0300

    finished cookies support usind Next GetServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,30 +4,58 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 
 import styles from '../styles/pages/Home.module.css'
 import ChallengeBox from "../components/ChallengeBox";
 import { CountdownProvider } from "../contexts/CountDownContext";
+import { ChallengesProvider } from "../contexts/ChallengeContext";
 
-export default function Home() {
+interface HomeProps {
+  level: number
+  currentExperience: number
+  challengesCompleted: number
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      <ExperienceBar />
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <CountDown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+        <ExperienceBar />
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <CountDown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+// Carrega na camada do nextjs(node) e entrega no componente como props
+// por exemplo chamadas api aqui irão carregar antes da renderização no browser
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
